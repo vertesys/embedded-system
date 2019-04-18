341c67c74bbabb313f60846b2bbad1a1150b3d0f
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-18 17:18:11 +0200

    Add function

--- robot.sh
+++ robot.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 ######################################################################################
+filename="robot.py"
+######################################################################################
 function robotusage() {
     echo -e "Usage :\n   $0 start|stop|restart|status\n" >&2 ; exit 0
 } 
 ######################################################################################
+function robotstatus() {
+    if (pgrep -f "$filename") > /dev/null ; then
+        echo "- Le robot est en fonctionnement." ; return 0
+    else
+        echo "- Le robot n'est pas en fonctionnement." ; return 1
+    fi
+}
+######################################################################################
 case "$1" in
-    *) robotusage ;;
+    status) robotstatus ;;
+         *) robotusage ;;
 esac
 ######################################################################################
 
