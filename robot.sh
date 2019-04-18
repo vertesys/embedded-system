e788439f8248e93e530ac77cea415978298cce35
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-18 17:29:34 +0200

    Add start & stop functions

--- robot.sh
+++ robot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ######################################################################################
-filename="robot.py"
+filename="programme.py"
 ######################################################################################
 function robotusage() {
     echo -e "Usage :\n   $0 start|stop|restart|status\n" >&2 ; exit 0
@@ -14,7 +14,30 @@ function robotstatus() {
     fi
 }
 ######################################################################################
+function robotstart() {
+    if ! robotstatus > /dev/null ; then
+        $(nohup python $filename > /dev/null 2>&1 &) ; sleep 1
+        if robotstatus > /dev/null ; then
+            echo "- Le robot a été démarré."
+        else 
+            echo "- Le robot n'a pas pu être démarré."
+        fi
+    else
+        sleep 1 ; echo "- Le robot a été déjà démarré."
+    fi
+}
+function robotstop() {
+    if robotstatus > /dev/null ; then 
+        pkill -f "$filename" > /dev/null 2>&1 ; sleep 1
+        echo "- Le robot a été arrêté."
+    else
+        sleep 1 ; echo "- Le robot a été déjà arrêté."
+    fi
+}
+######################################################################################
 case "$1" in
+      stop) robotstop ;;
+     start) robotstart ;;
     status) robotstatus ;;
          *) robotusage ;;
 esac
