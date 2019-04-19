bbf3ff835a5c44bf9faba40795df9548b4bff720
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-19 20:40:29 +0000

    Fix all issues

--- robot.sh
+++ robot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ######################################################################################
-sha_1_commit_ok=~/.sha-1-commit-ok ; touch $sha_1_commit_ok
-sha_1_commit_no=~/.sha-1-commit-no ; touch $sha_1_commit_no
-filename="programme.py"
+directory="." ; filename="Programme.py" ; mkdir ~/.robot/ > /dev/null 2>&1
+sha_1_robot_works=~/.robot/sha-1-robot-works  ; touch $sha_1_robot_works
+sha_1_robot_error=~/.robot/sha-1-robot-error  ; touch $sha_1_robot_error
 ######################################################################################
 function robotusage() {
     echo -e "Usage :\n   $0 auto|start|stop|restart|status\n" >&2 ; exit 0
@@ -18,21 +18,21 @@ function robotstatus() {
 ######################################################################################
 function robotstart() {
     if ! robotstatus > /dev/null ; then
-        $(nohup python $filename > /dev/null 2>&1 &) ; sleep 1
+        $(nohup python "$directory/$filename" > /dev/null 2>&1 &) ; sleep 1
         if robotstatus > /dev/null ; then
             echo "- Le robot a été démarré."
-            echo $(git rev-parse HEAD) > $sha_1_commit_ok
+            echo $(git -C $directory rev-parse HEAD) > $sha_1_robot_works
         else 
             echo "- Le robot n'a pas pu être démarré."
-            echo $(git rev-parse HEAD) > $sha_1_commit_no
+            echo $(git -C $directory rev-parse HEAD) > $sha_1_robot_error
         fi
     else
         sleep 1 ; echo "- Le robot a été déjà démarré."
     fi
 }
 function robotstop() {
-    if robotstatus > /dev/null ; then 
-        pkill -f "$filename" > /dev/null 2>&1 ; sleep 1
+    if robotstatus > /dev/null ; then
+        pkill -f "$dorectory/$filename" > /dev/null 2>&1 ; sleep 1
         echo "- Le robot a été arrêté."
     else
         sleep 1 ; echo "- Le robot a été déjà arrêté."
@@ -40,10 +40,13 @@ function robotstop() {
 }
 ######################################################################################
 function checkupdate() {
-    git fetch origin > /dev/null 2>&1
-    reslog=$(git log HEAD..origin/master --oneline) > /dev/null
-    if [ "${reslog}" != "" ] ; then
-        if [ "$(git rev-parse HEAD)" != "$(cat $sha_1_commit_no)" ] ; then
+    git -C $directory fetch origin > /dev/null 2>&1
+    results=$(git -C $directory log HEAD..origin/master --oneline) > /dev/null 2>&1
+    if [ "${results}" != "" ] ; then
+        if [ "$(cat $sha_1_robot_works)" == "" ] ; then 
+            echo "+ Mise à jour du robot disponible." ; return 0
+        fi
+        if [ "$(git -C $directory rev-parse HEAD)" != "$(cat $sha_1_robot_error)" ] ; then
             echo "+ Mise à jour du robot disponible." ; return 0
         fi
     fi
@@ -51,30 +54,27 @@ function checkupdate() {
 }
 function cancelupdate() {
     sleep 1; echo "+ Suppression de la mise à jour.";
-    (cat $sha_1_commit_ok | git reset --hard) > /dev/null
+    git -C $directory reset --hard $(cat $sha_1_robot_works) > /dev/null 2>&1
 }
 function applyupdate() {
-    sleep 1; echo "- Mise à jour en cours...";
-    git merge origin/master > /dev/null 2>&1
+    sleep 1; echo "- Application de la mise à jour.";
+    git -C $directory merge origin/master > /dev/null 2>&1
 }
 ######################################################################################
 function autoupdate() {
-    if robotstatus > /dev/null ; then 
-        robotstop > /dev/null; robotstart; 
-    fi
+    robotstop > /dev/null ; robotstart | tr - + ;
     while true ; do
         if checkupdate ; then
-            echo -n "  "; robotstop 
+            echo -n "  "; robotstop
             echo -n "  "; applyupdate
-            echo -n "  "; robotstart
-            sleep 1;
+            echo -n "  "; robotstart ; sleep 1
             if ! robotstatus > /dev/null ; then
                 echo -n "  "; cancelupdate
                 echo -n "    "; robotstart
             fi
         fi
         sleep 3
-    done   
+    done
 }
 ######################################################################################
 case "$1" in
