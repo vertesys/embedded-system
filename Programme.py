1945d2e847e15e52eea8a9205b507f29b4e260bb
EMIL Ben Djadid <Emil.Ben.Djadid@Gmail.com>
2019-04-23 00:31:19 +0200

    Small update content

--- Programme.py
+++ Programme.py
@@ -3,7 +3,8 @@ import os, time, datetime
 
 while (True):
     file = open(os.path.expanduser("~/.robot/programme.txt"), "w")
-	file.write(datetime.datetime.today().strftime("\tDATE : %d-%m-%Y\n"))
-	file.write(datetime.datetime.now().strftime("\tHOUR : [%H:%M:%S]"))
+    file.write(datetime.datetime.today().strftime("\tDATE : %d-%m-%Y\n"))
+    file.write(datetime.datetime.now().strftime("\tHOUR : [%H:%M:%S]"))
     file.close()
     time.sleep(1)
+
