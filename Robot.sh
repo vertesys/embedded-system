f190989e2c49d413895b8b48ba0cc064336e13ed
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-22 20:00:56 +0000

    Add function oneinstance

--- Robot.sh
+++ Robot.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 ######################################################################################
+function oneinstance() {
+    for pid in $(pgrep -f $(basename "$0")) ; do
+        if [ $$ -ne $pid ] ; then kill -9 $pid ; fi
+    done
+} ; oneinstance
 directory="." ; filename="Programme.py" ; mkdir ~/.robot/ > /dev/null 2>&1
 sha_1_robot_works=~/.robot/sha-1-robot-works  ; touch $sha_1_robot_works
 sha_1_robot_error=~/.robot/sha-1-robot-error  ; touch $sha_1_robot_error
