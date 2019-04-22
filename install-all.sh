c6f5e690aa384b5dab91d34d6066082bc075024a
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-22 21:33:48 +0000

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -4,3 +4,4 @@ if [ ! -f $private_key ]; then
     yes y | ssh-keygen -f $private_key -q -N "" > /dev/null
     ansible-playbook --user pi -k -b -i inventory.inv playbooks/install-user.yml
 fi
+ansible-playbook -u deploy -b --private-key $private_key -i inventory.inv playbook.yml
