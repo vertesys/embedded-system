6ce0ad74341c8f6032a0d0208c064351ba5c87e4
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-23 00:03:55 +0000

    Add an error

--- Programme.py
+++ Programme.py
@@ -1,6 +1,6 @@
 # Import os, time and datetime modules.
 import os, time, datetime
-
+ADD ERROR
 while (True):
     file = open(os.path.expanduser("~/.robot/programme.txt"), "w")
     file.write(datetime.datetime.today().strftime("\tDATE : %d-%m-%Y\n"))
