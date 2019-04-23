505bc84dc0f2c3489e6c383acf599aae5d528464
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-23 13:52:00 +0000

    Update Robot.sh

--- Robot.sh
+++ Robot.sh
@@ -68,7 +68,8 @@ function applyupdate() {
 }
 ######################################################################################
 function autoupdate() {
-    variables ; sleep 1 ; (robotstatus | tr - +) > $file_informations ; sleep 1
+    # variables ; sleep 1 ; 
+    (robotstatus | tr - +) > $file_informations ; sleep 1
     if checkupdate ; then
         echo -n "  " ; robotstop
         echo -n "  " ; applyupdate
