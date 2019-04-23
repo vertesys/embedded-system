7bad43ce46197fb664c8af77f65f92ec69505002
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-23 12:26:44 +0000

    Update Robot.sh

--- Robot.sh
+++ Robot.sh
@@ -3,6 +3,7 @@
 function oneinstance() {
     pids=$(ps aux | grep -i bash.*$(basename "$0") | grep -v grep | awk '{print $2}')
     for pid in $pids ; do
+        echo $pid 
         if [ $$ -ne $pid ] ; then kill -9 $pid > /dev/null 2>&1 ; fi
     done
 } ; oneinstance
