4eddf5ecedc37eecf6edbfb7d41a32b8e336f585
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-23 10:41:33 +0000

    Fix issue robotstarts script py (was not started)

--- Robot.sh
+++ Robot.sh
@@ -26,7 +26,7 @@ function robotstatus() {
 ######################################################################################
 function robotstart() {
     if ! robotstatus > /dev/null ; then
-        $(nohup python "$directory/$script_py" > /dev/null 2>&1 &) ; sleep 1
+        ($directory/$script_py &) ; sleep 1
         if robotstatus > /dev/null ; then
             echo "- Le robot a été démarré."
             echo $(git -C $directory rev-parse HEAD) > $sha_1_robot_works
