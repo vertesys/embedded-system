fad44d178b661667587deecb13ceb8b50a8ee209
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-24 14:49:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ while True:
 
     elif state == '1':
         if ecran == 1:
-            afecran.text('coucou virtus')
+            afecran.text('BEN DJADID')
             # afecran.image('cat.jpg')
             ecran = 0
 
