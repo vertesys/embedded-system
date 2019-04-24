989b7086d05d47d083b4e8fd4afaf18cb9702519
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-24 05:21:28 +0000

    Update Ansible/install-all.sh

--- Ansible/install-all.sh
+++ Ansible/install-all.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-private_key=~/.ssh/id_rsa.robot
-if [ ! -f $private_key ]; then
-    yes y | ssh-keygen -f $private_key -q -N "" > /dev/null
-    ansible-playbook --user pi -k -b -i inventory.inv playbooks/install-user.yml
-fi
-ansible-playbook -u deploy -b --private-key $private_key -i inventory.inv playbook.yml
+ansible-playbook -u pi -b --ask-pass -i inventory.inv playbook.yml
