52887be6b6f58e9108c66ff128b173e74eea0e42
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-24 06:06:58 +0000

    Update Robot.sh

--- Robot.sh
+++ Robot.sh
@@ -9,9 +9,9 @@ function oneinstance() {
 function variables() {
     directory="/home/pi/.robot/robot" ; script_py="Programme.py" ; 
     mkdir -p $directory > /dev/null 2>&1 ; chmod +x $($directory/$script_py)
-    sha_1_robot_works=$directory/../sha-1-robot-works ; touch $sha_1_robot_works
-    sha_1_robot_error=$directory/../sha-1-robot-error ; touch $sha_1_robot_error
-    file_informations=$directory/../informations ; > $file_informations
+    sha_1_robot_works=/home/pi/.robot/sha-1-robot-works ; touch $sha_1_robot_works
+    sha_1_robot_error=/home/pi/.robot/sha-1-robot-error ; touch $sha_1_robot_error
+    file_informations=/home/pi/.robot/informations ; > $file_informations
 } ; variables
 ######################################################################################
 function robotusage() {
