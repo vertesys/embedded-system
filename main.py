334e8cc068e775455646345fea6385a13eb3d63b
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-24 15:12:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ while True:
 
     elif state == '1':
         if ecran == 1:
-            afecran.text('ALLO LA POLICE !')
+            afecran.text('MARCHE')
             # afecran.image('cat.jpg')
             ecran = 0
 
