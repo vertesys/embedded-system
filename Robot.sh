24a2a27efbf8a4ea423689fac0f0949db8b6facb
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-24 08:19:33 +0000

    Update Robot.sh

--- Robot.sh
+++ Robot.sh
@@ -55,7 +55,7 @@ function checkupdate() {
     sha_1_last_commit_online=$(git -C $directory ls-remote $url_remote HEAD | cut -f1)
     if [ $sha_1_last_commit_online != $(git -C $directory rev-parse HEAD) ] ; then
         my_sha_1_robot_error=$(head -n 1 $sha_1_robot_error)
-        if [ $sha_1_last_commit_online != $my_sha_1_robot_error ] ; then
+        if [ $sha_1_last_commit_online != "$my_sha_1_robot_error" ] ; then
             echo "+ Mise à jour du robot disponible." ; return 0
         fi
     fi
