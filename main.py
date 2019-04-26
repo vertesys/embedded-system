4268eeebbd3f31d3bdf61d7a09cdb713e446bde3
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-26 09:03:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ while True:
     elif state == '1':
         if ecran == 1:
             #afecran.image('Bisounours.jpg')
-            afecran.text('4 Fantastics !')
+            afecran.text('4 DEMO !')
             ecran = 0
 
         va = []
