855627267e1e873a908bbce388c4945af075945e
EMIL Ben Djadid <Emil.Ben.Djadid@Gmail.com>
2019-04-28 19:48:41 +0200

    Update Robot.sh

--- Robot.sh
+++ Robot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-######################################################################################
+###########################################################################################
 function oneinstance() {
     pids=$(ps aux | grep -i bash.*$(basename "$0") | grep -v grep | awk '{print $2}')
     for pid in $pids ; do 
@@ -7,13 +7,13 @@ function oneinstance() {
     done
 } ; oneinstance
 function variables() {
-    directory="/home/pi/.fantastics/robot" ; script_py="main.py" ; 
+    directory="/home/pi/.fantastics/robot" ; script_py="main.py" ; branch="origin/devops"
     mkdir -p $directory > /dev/null 2>&1 ; chmod +x $directory/$script_py
     sha_1_robot_works=/home/pi/.fantastics/sha-1-robot-works ; touch $sha_1_robot_works
     sha_1_robot_error=/home/pi/.fantastics/sha-1-robot-error ; touch $sha_1_robot_error
     file_informations=/home/pi/.fantastics/informations ; cp /dev/null $file_informations
 } ; variables
-######################################################################################
+###########################################################################################
 function robotusage() {
     echo -e "Usage :\n   $0 auto|start|stop|restart|status\n" >&2 ; exit 0
 }
@@ -24,7 +24,7 @@ function robotstatus() {
         echo "- Le robot n'est pas en fonctionnement." ; return 1
     fi
 }
-######################################################################################
+###########################################################################################
 function robotstart() {
     if ! robotstatus > /dev/null ; then
         $(nohup python3 "$directory/$script_py" > /dev/null 2>&1 &) ; sleep 2
@@ -49,11 +49,11 @@ function robotstop() {
         sleep 2 ; echo "- Le robot a été déjà arrêté."
     fi
 }
-######################################################################################
+###########################################################################################
 function checkupdate() {
     url_remote=$(git -C $directory config --get remote.origin.url)
-    sha_1_last_commit_online=$(git -C $directory ls-remote $url_remote HEAD | cut -f1)
-    if [ $sha_1_last_commit_online != $(git -C $directory rev-parse HEAD) ] ; then
+    sha_1_last_commit_online=$(git -C $directory ls-remote $url_remote $branch | cut -f1)
+    if [ $sha_1_last_commit_online != $(git -C $directory rev-parse $branch) ] ; then
         my_sha_1_robot_error=$(head -n 1 $sha_1_robot_error)
         if [ $sha_1_last_commit_online != "$my_sha_1_robot_error" ] ; then
             echo "+ Mise à jour du robot disponible." ; return 0
@@ -69,10 +69,16 @@ function applyupdate() {
     sleep 2 ; echo "- Application de la mise à jour."
     (find $directory/.git/objects/ -size 0 -exec rm -f {} \;) > /dev/null 2>&1
     git -C $directory branch --set-upstream-to=origin/master master > /dev/null 2>&1
-    git -C $directory checkout . > /dev/null 2>&1
+    git -C $directory config user.name "Robot" 
+    git -C $directory config user.email "robot@fantastics.local"
+    git -C $directory checkout master > /dev/null 2>&1
+    git -C $directory fetch origin > /dev/null 2>&1
+    git -C $directory add . ; git commit -m "Resolved merge conflict."
     git -C $directory pull > /dev/null 2>&1
+    git -C $directory merge -X theirs $branch
+    git -C $directory merge devops
 }
-######################################################################################
+###########################################################################################
 function autoupdate() {
     (robotstatus | tr - +) > $file_informations ; sleep 1
     if checkupdate ; then
@@ -93,7 +99,7 @@ function autoupdate() {
         fi
     fi
 } >> /home/pi/.fantastics/informations
-######################################################################################
+###########################################################################################
 case "$1" in
        stop) robotstop ;;
        auto) autoupdate ;;
@@ -102,4 +108,4 @@ case "$1" in
     restart) robotstop; robotstart ;;
           *) robotusage ;;
 esac
-######################################################################################
+###########################################################################################
