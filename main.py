ec3cfb6db3c67bf9a52096a688fc03fa773bbe74
Ben Djadid Ramadani <emil.ben.djadid@gmail.com>
2019-04-29 12:21:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ while True:
 
     elif state == '1':
         if ecran == 1:
-            afecran.text('Je suis là.'
+            afecran.text('Je suis fantastics.')
             # afecran.image('cat.jpg')
             ecran = 0
 
