fecffe8a80ee119d44dc54c1e35bbc284aafb680
EMIL Ben Djadid <Emil.Ben.Djadid@Gmail.com>
2019-04-30 07:38:08 +0200

    Improve and fix issues for monitoring tasks

--- Ansible/roles/monitoring/client/files/Monitoring.sh
+++ Ansible/roles/monitoring/client/files/Monitoring.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 count=0
 while [ $count -le 11 ] ; do
-    # chmod +x /home/pi/.fantastics/monitoring/eZServerMonitor.sh
-    /home/pi/.fantastics/monitoring/eZServerMonitor.sh -a > /home/pi/.fantastics/monitoring/temporary.html
-    # chmod +x /home/pi/.fantastics/monitoring/Convert.sh
-    sleep 2
+    content=$(/home/pi/.fantastics/monitoring/eZServerMonitor.sh -a)
+    echo $content > /home/pi/.fantastics/monitoring/temporary.html
     /home/pi/.fantastics/monitoring/Convert.sh
-    # chmod +x /home/pi/.fantastics/monitoring/Configure.sh
-    /home/pi/.fantastics/monitoring/Configure.sh
+    # $(/home/pi/.fantastics/monitoring/Configure.sh > /dev/null 2>&1)
+    sh /home/pi/.fantastics/monitoring/Configure.sh > /home/pi/.fantastics/monitoring/infos.txt
     count=$[$count+1]
+    sleep 1
 done
