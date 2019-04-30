ed5314488c58b3d622c77ea3bab1b4d7969f05e0
virtus1er <sbesseron@ajc-formation.fr>
2019-04-30 10:05:19 +0200

    robot v1

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ while True:
 
     elif state == '1':
         if ecran == 1:
-            afecran.text('Je suis fantastics...')
+            afecran.text('Je suis fantastics... virtus')
             # afecran.image('cat.jpg')
             ecran = 0
 
