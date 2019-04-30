353033c37d0a8bfe62a51dcb79b2b8b7b89da665
virtus1er <sbesseron@ajc-formation.fr>
2019-04-30 14:00:02 +0200

    robot v1

--- main.py
+++ main.py
@@ -31,64 +31,64 @@ while True:
             # rmot.tmoteur(90)
         va = []
 
-        if objetDistance.distance() < 35:
-            Vconteur = 3
-            Position = 0
-            conteur = 0
-        elif Position == 0:
-
-            if conteur == 0:
-                mov.Ravancer(False, True, True, False, 0.8, 0.8)
-                va.append(objetDistance.distance())
-
-            if objetDistance.distance() > min(va):
-                conteur = 1
-                Position = 1
-                Vconteur = 0
-                mov.Ravancer(False, False, False, False, 0, 0)
-                sleep(1)
-
-        elif Vconteur == 0:
-            caldis = (objetDistance.distance())
-            Vconteur = 1
-
-        elif Vconteur == 1:
-
-            if objetDistance.distance() <= caldis:
-                mov.Ravancer(False, False, False, False, 0, 0)
-                Vconteur = 3
-                Position = 0
-                conteur = 0
-                sleep(1)
-            else:
-                # mov.avancer()
-                mov.Ravancer(True, False, True, False, 0.8, 0.8)
-                sleep(2)
-                mov.Ravancer(True, False, False, True, 0.8, 0.8)
-                sleep(2)
-                mov.Ravancer(False, True, True, False, 0.8, 0.8)
-                sleep(4)
-                mov.Ravancer(True, False, False, True, 0.8, 0.8)
-                sleep(2)
-                mov.Ravancer(True, False, True, False, 0.8, 0.8)
-                sleep(1)
-
-                mov.Ravancer(True, False, False, True, 0.8, 0.8)
-                sleep(4)
-                # rmot.tmoteur(90)
-                # rmot.tmoteur(45)
-                # rmot.tmoteur(125)
-
-                mov.Ravancer(False, True, True, False, 0.8,0.8)
-                sleep(2)
-                mov.Ravancer(True, False, True, False, 0.8, 0.8)
-                sleep(2)
-                mov.Ravancer(False, True, False, True, 0.8, 0.8)
-                sleep(2)
-                mov.Ravancer(True, False, True, False, 0.8, 0.8)
-                sleep(2)
-                mov.Ravancer(False, True, True, False, 0.8, 0.8)
-                sleep(3)
+        # if objetDistance.distance() < 35:
+        #     Vconteur = 3
+        #     Position = 0
+        #     conteur = 0
+        # elif Position == 0:
+        #
+        #     if conteur == 0:
+        #         mov.Ravancer(False, True, True, False, 0.8, 0.8)
+        #         va.append(objetDistance.distance())
+        #
+        #     if objetDistance.distance() > min(va):
+        #         conteur = 1
+        #         Position = 1
+        #         Vconteur = 0
+        #         mov.Ravancer(False, False, False, False, 0, 0)
+        #         sleep(1)
+        #
+        # elif Vconteur == 0:
+        #     caldis = (objetDistance.distance())
+        #     Vconteur = 1
+        #
+        # elif Vconteur == 1:
+        #
+        #     if objetDistance.distance() <= caldis:
+        #         mov.Ravancer(False, False, False, False, 0, 0)
+        #         Vconteur = 3
+        #         Position = 0
+        #         conteur = 0
+        #         sleep(1)
+        #     else:
+        #         # mov.avancer()
+        #         mov.Ravancer(True, False, True, False, 0.8, 0.8)
+        #         sleep(2)
+        #         mov.Ravancer(True, False, False, True, 0.8, 0.8)
+        #         sleep(2)
+        #         mov.Ravancer(False, True, True, False, 0.8, 0.8)
+        #         sleep(4)
+        #         mov.Ravancer(True, False, False, True, 0.8, 0.8)
+        #         sleep(2)
+        #         mov.Ravancer(True, False, True, False, 0.8, 0.8)
+        #         sleep(1)
+        #
+        #         mov.Ravancer(True, False, False, True, 0.8, 0.8)
+        #         sleep(4)
+        #         # rmot.tmoteur(90)
+        #         # rmot.tmoteur(45)
+        #         # rmot.tmoteur(125)
+        #
+        #         mov.Ravancer(False, True, True, False, 0.8,0.8)
+        #         sleep(2)
+        #         mov.Ravancer(True, False, True, False, 0.8, 0.8)
+        #         sleep(2)
+        #         mov.Ravancer(False, True, False, True, 0.8, 0.8)
+        #         sleep(2)
+        #         mov.Ravancer(True, False, True, False, 0.8, 0.8)
+        #         sleep(2)
+        #         mov.Ravancer(False, True, True, False, 0.8, 0.8)
+        #         sleep(3)
 
 
 
